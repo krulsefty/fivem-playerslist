88916bf5b9c97ee342ef35d96148fd269cf4f64f
sefty <68341055+krulsefty@users.noreply.github.com>
2024-09-16 19:26:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -41,6 +41,7 @@ document.onkeydown = function (e) {
 
 let playersInfo = []
 let searchResults = []
+let currentSort = 'id'
 
 async function displayPlayers() {
   try {
@@ -51,27 +52,40 @@ async function displayPlayers() {
     const data = await response.json()
 
     playersInfo = data.Data.players.map(player => ({
-      name: player.name.replace(/exilerp(\.eu)?/gi, ''),
-      id: player.id,
+      name: `${player.name.replace(/exilerp(\.eu)?/gi, '')}`,
+      id: `<span class="colored-text">${player.id}</span>`,
       identifiers: player.identifiers.map(identifier => identifier.toLowerCase())
     }))
 
     if (data.error) {
       document.getElementById("list").innerHTML = "Serwer jest wylaczony."
-      document.getElementById("count").remove()
+      document.getElementById("count").style.display = 'none'
+      document.getElementById("sort").style.display = 'none'
+      document.getElementById("dupa").style.display = 'none'
+    } else {
+      document.getElementById("count").style.display = 'block'
+      document.getElementById("sort").style.display = 'block'
+      document.getElementById("dupa").style.display = 'flex'
     }
 
-    playersInfo.sort((a, b) => a.id - b.id)
+    if (currentSort === 'name') {
+      playersInfo.sort((a, b) => a.name.localeCompare(b.name))
+    } else {
+      playersInfo.sort((a, b) => a.id - b.id)
+    }
 
     if (searchResults.length > 0) {
       updateDisplay(searchResults)
     } else {
       updateDisplay(playersInfo)
     }
+
   } catch (error) {
     console.error('Error fetching player data:', error)
     document.getElementById("list").innerHTML = "Nie udalo sie zaladowac listy gracz.<br>Sprobojemy ja wyswietlnic ponownie, moze to potrwac kilka sekund."
-    document.getElementById("count").remove()
+    document.getElementById("count").style.display = 'none'
+    document.getElementById("sort").style.display = 'none'
+    document.getElementById("dupa").style.display = 'none'
   }
 
   const response = await fetch('https://servers-frontend.fivem.net/api/servers/single/y5k9om')
@@ -80,18 +94,20 @@ async function displayPlayers() {
   const playersCount = data.Data.clients
   const maxPlayers = data.Data.sv_maxclients
   const queue = data.Data.vars.Kolejka
+
   document.getElementById("count").innerHTML = `Obecnie graczy: <span class="colored-text">${playersCount}</span> / ${maxPlayers} [+ ${queue}]`
 }
 
-function searchPlayers() {
-  const searchTerm = document.getElementById("searchInput").value.toLowerCase().trim()
-  searchResults = playersInfo.filter(player =>
-    player.name.toLowerCase().includes(searchTerm) ||
-    player.id.toString().includes(searchTerm) ||
-    player.identifiers.some(identifier => identifier.includes(searchTerm))
-  )
+function SortButtonAbc() {
+  currentSort = 'name'
+  playersInfo.sort((a, b) => a.name.localeCompare(b.name))
+  updateDisplay(playersInfo)
+}
 
-  updateDisplay(searchResults)
+function SortButtonId() {
+  currentSort = 'id'
+  playersInfo.sort((a, b) => a.id - b.id)
+  updateDisplay(playersInfo)
 }
 
 function updateDisplay(players) {
@@ -107,3 +123,4 @@ document.getElementById("searchInput").addEventListener("keypress", function (ev
 
 displayPlayers()
 setInterval(displayPlayers, 1000)
+
